ba1b8a0ddcafa26e32687fbbe536fdc0f80b7910
mbernardes <miria.bernardes@mercadolivre.com>
2021-07-15 17:26:56 -0300

    Implementando BairrosController

--- src/main/java/br/com/bootcamp/desafio_quality/controller/BairrosController.java
+++ src/main/java/br/com/bootcamp/desafio_quality/controller/BairrosController.java
@@ -1,9 +1,45 @@
 package br.com.bootcamp.desafio_quality.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.bootcamp.desafio_quality.dto.BairroDTO;
+import br.com.bootcamp.desafio_quality.service.IBairrosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/bairros")
 public class BairrosController {
+
+    private final IBairrosService bairrosService;
+
+    @Autowired
+    public BairrosController(IBairrosService bairrosService) {
+        this.bairrosService = bairrosService;
+    }
+
+
+    @GetMapping("/{nome}")
+    public BairroDTO buscarBairro(@PathVariable  String nome) {
+        return bairrosService.buscarBairro(nome);
+    }
+
+    @PostMapping
+    public ResponseEntity<BairroDTO> persistirBairro(@RequestBody @Valid BairroDTO bairro) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bairrosService.persisteBairro(bairro));
+    }
+
+    @DeleteMapping("/{nome}")
+    ResponseEntity<?> deletarBairro(@PathVariable String nome) {
+        bairrosService.deletarBairro(nome);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping
+    public BairroDTO alterarBairro(@RequestBody @Valid BairroDTO bairro) {
+        return bairrosService.alterarBairro(bairro);
+    }
 }
